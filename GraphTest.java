84542efcbc9662e2670866dc0de9388845aee979
Yingjie Shen <shen92@wisc.edu>
2019-04-18 23:29:16 -0500

    13.5/15 all done

--- GraphTest.java
+++ GraphTest.java
@@ -1,4 +1,38 @@
-
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title: p4_project
+// Files: CycleException.java, Graph.java, GraphADT.java, GraphTest.java,
+// Package.java, PackageManager.java, PackageManagerTest.java,
+// PackageNotFoundException.java
+// Course: CS 400 Spring 2019 002
+//
+// Author: Yingjie Shen
+// Email: shen92@wisc.edu
+// Lecturer's Name: Deb Deppeler
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+//
+// Partner Name: Dongxia Wu
+// Partner Email: dwu93@wisc.edu
+// Partner Lecturer's Name: Deb Deppeler
+//
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
+// ___ Write-up states that pair programming is allowed for this assignment.
+// ___ We have both read and understand the course Pair Programming Policy.
+// ___ We have registered our team prior to the team registration deadline.
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully
+// acknowledge and credit those sources of help here. Instructors and TAs do
+// not need to be credited here, but tutors, friends, relatives, room mates,
+// strangers, and others do. If you received no outside help from either type
+// of source, then please explicitly indicate NONE.
+//
+// Persons: (identify each person and describe their help in detail)
+// Online Sources: (identify each URL and describe their assistance in detail)
+//
+/////////////////////////////// 80 COLUMNS WIDE ///////////////////////////////
 import static org.junit.jupiter.api.Assertions.*; // org.junit.Assert.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
